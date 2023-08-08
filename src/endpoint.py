1acc4a89b4b45c958ea573c09c7f3b236d0e5f09
Ars Vladimirov <grokhael@gmail.com>
2023-08-08 22:51:05 +0600

    run

--- src/endpoint.py
+++ src/endpoint.py
@@ -30,60 +30,31 @@ app = FastAPI()
 
 @app.get("/test")
 def test():
-    return "testt333"
+    return "test4"
 
 
 @app.get("/renders", response_class=Response)
 async def image_endpoint(project_id: int, image_id: int):
     OUTPUT_WIDTH_PX = settings_dict.get("OUTPUT_WIDTH_PX", 500)
-    BBOX_THICKNESS_PERCENT = settings_dict.get("BBOX_THICKNESS_PERCENT", 0.005)
+    BBOX_THICKNESS_PERCENT = settings_dict.get("BBOX_THICKNESS_PERCENT", 0.05)
     BBOX_OPACITY = 1
     FILLBBOX_OPACITY = settings_dict.get("FILLBBOX_OPACITY", 0.2)
     MASK_OPACITY = settings_dict.get("MASK_OPACITY", 0.7)
 
-    # project_id = g.api.image.get_project_id(image_id)
-    project_meta = sly.ProjectMeta.from_json(g.JSON_METAS[project_id])
-    # project_meta = sly.ProjectMeta.from_json(g.api.project.get_meta(project_id))
+    # project_meta = sly.ProjectMeta.from_json(g.JSON_METAS[project_id])
+    project_meta = sly.ProjectMeta.from_json(g.api.project.get_meta(project_id))
 
     jann = g.api.annotation.download_json(image_id)
     ann = sly.Annotation.from_json(jann, project_meta)
 
-    # for image, ann in zip([image], [ann]):
+    # for ann in [ann]:
     rgba = u.get_rgba_np(
         ann, OUTPUT_WIDTH_PX, BBOX_THICKNESS_PERCENT, BBOX_OPACITY, FILLBBOX_OPACITY, MASK_OPACITY
     )
 
-    arr = cv2.cvtColor(rgba.astype("uint8"), cv2.COLOR_RGBA2BGRA)
+    rgba = cv2.cvtColor(rgba.astype("uint8"), cv2.COLOR_RGBA2BGRA)
 
-    success, im = cv2.imencode(".png", arr)
-    # headers = {"Content-Disposition": 'inline; filename="test.png"'}
-    headers = {"Cache-Control": "max-age=604800", "Content-Type": "image/png"}
-    return Response(im.tobytes(), headers=headers, media_type="image/png")
+    success, im = cv2.imencode(".png", rgba)
 
-
-@app.post("/renders_", response_class=Response)
-async def image_endpoint(project_id: int, image_id: int):
-    OUTPUT_WIDTH_PX = settings_dict.get("OUTPUT_WIDTH_PX", 500)
-    BBOX_THICKNESS_PERCENT = settings_dict.get("BBOX_THICKNESS_PERCENT", 0.005)
-    BBOX_OPACITY = 1
-    FILLBBOX_OPACITY = settings_dict.get("FILLBBOX_OPACITY", 0.2)
-    MASK_OPACITY = settings_dict.get("MASK_OPACITY", 0.7)
-
-    # project_id = g.api.image.get_project_id(image_id)
-    project_meta = sly.ProjectMeta.from_json(g.JSON_METAS[project_id])
-    # project_meta = sly.ProjectMeta.from_json(g.api.project.get_meta(project_id))
-
-    jann = g.api.annotation.download_json(image_id)
-    ann = sly.Annotation.from_json(jann, project_meta)
-
-    # for image, ann in zip([image], [ann]):
-    rgba = u.get_rgba_np(
-        ann, OUTPUT_WIDTH_PX, BBOX_THICKNESS_PERCENT, BBOX_OPACITY, FILLBBOX_OPACITY, MASK_OPACITY
-    )
-
-    arr = cv2.cvtColor(rgba.astype("uint8"), cv2.COLOR_RGBA2BGRA)
-
-    success, im = cv2.imencode(".png", arr)
-    # headers = {"Content-Disposition": 'inline; filename="test.png"'}
     headers = {"Cache-Control": "max-age=604800", "Content-Type": "image/png"}
     return Response(im.tobytes(), headers=headers, media_type="image/png")
--- src/main.py
+++ src/main.py
@@ -1,8 +1,12 @@
+import multiprocessing
 import os
 from pathlib import Path
 
+import uvicorn
+
 import src.globals as g
 import supervisely as sly
+from src.endpoint import app as endpoint_app
 from src.ui import card_1
 from supervisely.app.widgets import Container
 
@@ -10,3 +14,14 @@ layout = Container(widgets=[card_1], direction="vertical")
 
 static_dir = Path(g.STORAGE_DIR)
 app = sly.Application(layout=layout, static_dir=static_dir)
+
+
+def run_endpoint_app():
+    uvicorn.run(endpoint_app, host="0.0.0.0", port=8000)
+
+
+if __name__ == "__main__":
+    endpoint_process = multiprocessing.Process(target=run_endpoint_app)
+
+    endpoint_process.start()
+    endpoint_process.join()
